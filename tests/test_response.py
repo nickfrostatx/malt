1bb934bf337a26a96b8f1b5a4237411340b03572
Nick Frost <nickfrostatx@gmail.com>
2015-12-28 15:11:18 -0600

    Test encoding functionality

--- tests/test_response.py
+++ tests/test_response.py
@@ -29,10 +29,25 @@ def test_status_invalid():
 
 def test_response_iterable():
     assert Response('abc').response == ['abc']
-    assert Response([b'abc', 'def']).response == [b'abc', 'def']
+    assert Response([b'abc', 'déf']).response == [b'abc', 'déf']
 
 
 def test_response_text():
     assert list(Response(b'Some text')) == [b'Some text']
-    assert list(Response('Some text')) == [b'Some text']
-    assert list(Response([b'abc', 'def'])) == [b'abc', b'def']
+    assert list(Response(u'Some text')) == [b'Some text']
+    assert list(Response([b'abc', 'déf'])) == [b'abc', b'd\xc3\xa9f']
+
+
+def test_unicode_response():
+    assert list(Response(u'こんにちは')) == [
+        b'\xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf']
+
+
+def test_alternate_charsets():
+    response = list(Response(u'böse', charset='latin')) == [b'b\xf6se']
+
+    with pytest.raises(UnicodeEncodeError):
+        list(Response(u'こんにちは', charset='latin'))
+
+    assert list(Response(u'こんにちは', charset='euc-jp')) == [
+        b'\xa4\xb3\xa4\xf3\xa4\xcb\xa4\xc1\xa4\xcf']
