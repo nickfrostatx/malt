79ecbb4d6da944dd1fc0c4b3b77fe67d600af42d
Nick Frost <nickfrostatx@gmail.com>
2016-01-01 16:47:15 -0600

    Happy New Year! :sparkles:

--- malt/__init__.py
+++ malt/__init__.py
@@ -2,7 +2,7 @@
 """
 Minimalistic WSGI framework.
 
-:copyright: (c) 2015 by Nicholas Frost.
+:copyright: (c) 2016 by Nicholas Frost.
 :license: MIT, see LICENSE for more details.
 """
 
@@ -12,7 +12,7 @@ from .util import json_response as json
 from .wrappers import Request, Response
 
 __author__ = 'Nick Frost'
-__copyright__ = 'Copyright 2015, Nicholas Frost'
+__copyright__ = 'Copyright 2016, Nicholas Frost'
 __license__ = 'MIT'
 __version__ = '0.1.dev0'
 __email__ = 'nickfrostatx@gmail.com'
