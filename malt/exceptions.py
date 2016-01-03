25e02b4d0973280e1ac3d5caedbd56fbc201fc7a
Nick Frost <nickfrostatx@gmail.com>
2016-01-03 08:11:17 -0600

    Ensure a valid status code in HTTPException

--- malt/exceptions.py
+++ malt/exceptions.py
@@ -2,17 +2,18 @@
 """HTTP exception object."""
 
 from .http import HTTP_STATUS_CODES
-from .wrappers import Response
 
 
 class HTTPException(Exception):
 
     """This can be raised from middleware to render an error page."""
 
-    def __init__(self, status_code, message=None, exception=None):
+    def __init__(self, code=None, message=None, exception=None):
+        if not isinstance(code, int) or code not in HTTP_STATUS_CODES:
+            code = 500
         if message is None:
-            message = HTTP_STATUS_CODES[status_code]
+            message = HTTP_STATUS_CODES[code]
         super(HTTPException, self).__init__(message)
-        self.status_code = status_code
+        self.status_code = code
         self.message = message
         self.exception = exception
