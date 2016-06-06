f89f53f89de7ffe20b13ff114f72d515ace92e2b
Nick Frost <nickfrostatx@gmail.com>
2016-06-05 21:36:31 -0700

    Augment json_response

--- malt/util.py
+++ malt/util.py
@@ -6,8 +6,20 @@ from .http import MIME_JSON
 from .wrappers import Response
 
 
-def json_response(data, **kwargs):
+def json_response(data, pretty=False, **kwargs):
     kwargs.setdefault('mimetype', MIME_JSON)
-    text = dumps(data, separators=(',', ':'), ensure_ascii=False,
-                 sort_keys=True)
+    if pretty:
+        kwargs.setdefault('ensure_ascii', True)
+        kwargs.setdefault('separators', None)
+        kwargs.setdefault('indent', 2)
+    else:
+        kwargs.setdefault('ensure_ascii', False)
+        kwargs.setdefault('separators', (',', ':'))
+        kwargs.setdefault('indent', None)
+
+    text = dumps(data,
+                 ensure_ascii=kwargs.pop('ensure_ascii'),
+                 separators=kwargs.pop('separators'),
+                 indent=kwargs.pop('indent'),
+                 sort_keys=True) + u'\n'
     return Response(text, **kwargs)
